4a35184a4f2e3d3fd6d25758742f8a30512aa01c
Michael Navazhylau <mikipux7@gmail.com>
2017-07-22 19:40:03 -0500

    removed styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,11 +20,7 @@ import { PostsService } from './posts/posts.service';
   ],
   template: `
     <header>
-    <md-toolbar color="primary">
-     <a [routerLink]="['/']" class="logotTxt">MEAN</a>
-   <a class="links" [routerLink]="['/posts']">Posts</a>
-     <span class="links">more...</span>
-    </md-toolbar>
+
     </header>
       <router-outlet></router-outlet>
     <footer>
